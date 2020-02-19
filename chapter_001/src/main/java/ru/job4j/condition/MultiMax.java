647b9c4db0451055162bf7fa3867d1f3162189a1
sergiushtudner <sergiu.shtudner@gmail.com>
2020-02-20 01:30:53 +0200

    Max from 3 numbers [#234676]

---
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+// Максимум из трех чисел[#234676]
+
+public class MultiMax {
+    public int max(int first, int second, int third) {
+        int result1 = (first > second) && (first > third) && (second > third)? first : second;
+        int result2 = (result1 > third) ? result1 : third;
+        return result2;
+
+    }
+}
---
+++ chapter_001/src/test/java/MultiMaxTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import ru.job4j.condition.MultiMax;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MultiMaxTest {
+    @Test
+    public void whenSecondMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(1, 4, 2);
+        assertThat(result, is(4));
+    }
+}
