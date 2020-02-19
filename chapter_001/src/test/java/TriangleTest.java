065cfba6e14e1337b45f19a38003c934f26125df
sergiushtudner <sergiu.shtudner@gmail.com>
2020-02-20 01:05:25 +0200

    Triangle is exist? [#234678]

---
+++ chapter_001/src/test/java/TriangleTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import ru.job4j.condition.Triangle;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TriangleTest {
+    @Test
+    public void whenExist() {
+        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        assertThat(result, is(true));
+    }
+
+}
