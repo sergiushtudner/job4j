03631044be1932fd36d5c720df9333443bbc8251
sergiushtudner <sergiu.shtudner@gmail.com>
2020-02-25 13:35:59 +0200

    JavaDoc. Code convention[#234760]

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -1,11 +1,29 @@
 package ru.job4j.loop;
-//В этом задании нужно посчитать количество лет необходимых для погашения кредита.
-//Кредитная сумма перерасчитывается каждый год от остатка суммы умноженной на процент.
-public class Mortgage {
+/**
+ * Mortgage calc
+ *
+ * @author Sergiu Studner (sergiu.shtudner@gmail.com)
+ * В этом задании нужно посчитать количество лет необходимых для погашения кредита.
+ * Кредитная сумма перерасчитывается каждый год от остатка суммы умноженной на процент.
+*/
+ public class Mortgage {
+    /**
+     * year - calculates number of years for paying amount with percent from salary
+     * @param amount - amount
+     * @param salary - salary
+     * @param percent - percent
+     */
     public int year(double amount, double salary, double percent) {
+        /*
+         * Contains started numbers of years from 0
+         */
         int year = 0;
+
         while (0 < (amount + amount * (percent / 100))) {
             amount = (amount + amount * (percent / 100)) - salary;
+            /*
+             * Contains calculated numbers of years
+             */
             year++;
 
         }   return year;
---
+++ chapter_001/src/main/java/ru/job4j/loop/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for Mortgage task.
+ *
+ * @author Sergiu Studner (sergiu.shtudner@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.loop;
