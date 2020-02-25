4dea1407662d81c821a5babb8f3daf862f8c5cf6
sergiushtudner <sergiu.shtudner@gmail.com>
2020-02-25 12:52:46 +0200

    Mortgage calc (Ипотека).[#234752]

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,8 +3,8 @@ package ru.job4j.loop;
 //Кредитная сумма перерасчитывается каждый год от остатка суммы умноженной на процент.
 public class Mortgage {
     public int year(double amount, double salary, double percent) {
-        int year = 1;
-        while (salary < (amount + amount * (percent / 100))) {
+        int year = 0;
+        while (0 < (amount + amount * (percent / 100))) {
             amount = (amount + amount * (percent / 100)) - salary;
             year++;
 
