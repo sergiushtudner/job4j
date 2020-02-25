ac2a03faf05bf68370f056f7c16b9158f0a45354
sergiushtudner <sergiu.shtudner@gmail.com>
2020-02-25 02:34:05 +0200

    Simple numbers.[#234751]

---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+//В этом задании нужно посчитать количество простых чисел от 1 до x.
+// Простое число считается, число которое делится только на себя и на единицу.
+public class PrimeNumber {
+    public int calc(int finish) {
+        int count = 0;
+        for (int number = 1; number <= finish; number++) {
+            if (CheckPrimeNumber.check(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
