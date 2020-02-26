732401b53cfbc1c65e5726d525105b40110f4db2
sergiushtudner <sergiu.shtudner@gmail.com>
2020-02-26 04:12:59 +0200

    Cross (X) in pseudographics. [# 234754]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -0,0 +1,33 @@
+package ru.job4j.loop;
+// нужно нарисовать крест в консоле.
+public class Slash {
+  public static void draw(int size) {
+       for (int row = 0; row < size; row++) {
+           for (int cell = 0; cell < size; cell++) {
+//              boolean left = (row - cell) == 0; // добавить условие, по которому нужно определить ставить ли символ или нет.
+//              boolean right = ((row + cell) == size-1); // добавить условие, что нужно ставить элемент в правый угол.
+               boolean left = (row==cell); // добавить условие, по которому нужно определить ставить ли символ или нет.
+               boolean right = ((row + cell) == size - 1); // добавить условие, что нужно ставить элемент в правый угол.
+
+//               boolean left = ((cell + row) % 2 == 0); // добавить условие, по которому нужно определить ставить ли символ или нет.
+//               boolean right = ((cell + row) % 2 == 0); // добавить условие, что нужно ставить элемент в правый угол.
+               if (left) {
+                   System.out.print("0");
+               } else if (right) {
+                   System.out.print("0");
+               } else {
+                   System.out.print(" ");
+               }
+           }
+           System.out.println();
+       }
+       System.out.println();
+   }
+
+   public static void main(String[] args) {
+       System.out.println("Draw by 3");
+      draw(3);
+       System.out.println("Draw by 5");
+       draw(5);
+   }
+}
