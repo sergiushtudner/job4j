62f1e6d35024997e3930fd6f0ee5d0bb7d784a3d
sergiushtudner <sergiu.shtudner@gmail.com>
2020-02-26 16:25:30 +0200

    6.0.2. Array filling.[#234695]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] flags = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Serj Studner";
+        names[2] = "Sergiu Studner";
+        names[3] = "Serghei Studner";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
