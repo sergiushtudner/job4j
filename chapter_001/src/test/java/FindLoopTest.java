6a90a32260e9b5cfa4207727701c103ab9902930
sergiushtudner <sergiu.shtudner@gmail.com>
2020-02-27 03:19:27 +0200

    Classic search with sorting out. [# 234681]

---
+++ chapter_001/src/test/java/FindLoopTest.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+import ru.job4j.array.FindLoop;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHas10Then1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 10;
+        int result = find.indexOf(input, value);
+        int expect = 1;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHas4Then3() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 4;
+        int result = find.indexOf(input, value);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHas7Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3, 7};
+        int value = 7;
+        int result = find.indexOf(input, value);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+}
