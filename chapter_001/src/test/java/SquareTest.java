a1e698af311b8dffd4c8e02f76763ec9281598df
sergiushtudner <sergiu.shtudner@gmail.com>
2020-02-27 02:56:09 +0200

    6.0.5. Array in tests.[#234697]

---
+++ chapter_001/src/test/java/SquareTest.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+import ru.job4j.array.Square;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SquareTest {
+    @Test
+    public void whenBound3Then014() {
+        int bound = 3;
+        int[] rst = Square.calculate(bound);
+        int[] expect = new int[] {0, 1, 4};
+        assertThat(rst, is(expect));
+    }
+    @Test
+    public void whenBound4Then0149() {
+        int bound = 4;
+        int[] rst = Square.calculate(bound);
+        int[] expect = new int[] {0, 1, 4, 9};
+        assertThat(rst, is(expect));
+    }
+    @Test
+    public void whenBound4Then014916() {
+        int bound = 5;
+        int[] rst = Square.calculate(bound);
+        int[] expect = new int[] {0, 1, 4, 9, 16};
+        assertThat(rst, is(expect));
+    }
+    @Test
+    public void whenBound4Then01491625() {
+        int bound = 6;
+        int[] rst = Square.calculate(bound);
+        int[] expect = new int[] {0, 1, 4, 9, 16, 25};
+        assertThat(rst, is(expect));
+    }
+}
