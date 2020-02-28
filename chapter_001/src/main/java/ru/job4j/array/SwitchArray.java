0e572154923c39bad5578873b15c7b828cc3bd86
sergiushtudner <sergiu.shtudner@gmail.com>
2020-02-28 04:28:28 +0200

    6.2.1. Exit out of border of array.[#234699]

---
+++ chapter_001/src/main/java/ru/job4j/array/SwitchArray.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+/**
+ * Метод swapBorder - меняет местами нулевой элемент и последний элемент в массиве.
+ * Это код падает с ошибкой java.lang.ArrayIndexOutOfBoundsException.
+ * Ваша задача устранить эту ошибку.
+ */
+public class SwitchArray {
+    public static int[] swapBorder(int[] array) {
+        int temp = array[0];
+//        array[0] = array[array.length];
+        array[0] = array.length;
+//        array[array.length] = temp;
+        array[array.length-1] = temp;
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {1, 2, 3, 4, 5, 6};
+        int[] rsl = swapBorder(nums);
+        for (int index = 0; index < rsl.length; index++) {
+            System.out.println(rsl[index]);
+        }
+    }
+}
