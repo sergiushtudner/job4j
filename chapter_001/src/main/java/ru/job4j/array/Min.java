880a9a18900667697ed3a253b26f6191d75ffe3d
sergiushtudner <sergiu.shtudner@gmail.com>
2020-03-03 16:22:16 +0200

    6.5.1. Find min number in Array.[#234690]

---
+++ chapter_001/src/main/java/ru/job4j/array/Min.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+/**
+ * В этом задании нужно найти минимальное число в массива
+ */
+public class Min {
+    public static int findMin(int[] array) {
+        int min = array[0];
+        for (int index = 0; index < array.length; index++) {
+            // проверить, что эталон больше, чем элемент. записать в эталон элемент массива.
+            if (array[index] < min){
+                min = array[index];
+            }
+        }
+        return min;
+    }
+}
---
+++ chapter_001/src/test/java/array/MinTest.java
@@ -0,0 +1,48 @@
+package array;
+
+import org.junit.Test;
+import ru.job4j.array.Min;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class MinTest {
+    @Test
+    public void whenFirstMin() {
+        assertThat(
+                Min.findMin(
+                        new int[] {0, 5, 10}
+                ),
+                is(0)
+        );
+    }
+
+    @Test
+    public void whenLastMin() {
+        assertThat(
+                Min.findMin(
+                        new int[] {10, 5, 3}
+                ),
+                is(3)
+        );
+    }
+
+    @Test
+    public void whenMiddleMin() {
+        assertThat(
+                Min.findMin(
+                        new int[] {10, 2, 5}
+                ),
+                is(2)
+        );
+    }
+    @Test
+    public void whenMin() {
+        assertThat(
+                Min.findMin(
+                        new int[] {2,5,78,1,8,345,78,45,9}
+                ),
+                is(1)
+        );
+    }
+}
