c0d2d5bfe8f96b3aff1f441afa522e6de619edf5
sergiushtudner <sergiu.shtudner@gmail.com>
2020-03-04 03:55:38 +0200

    6.5.3. Sorting array ascending elements.[#234689]

---
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+/**
+ * Сортировка упорядочивает массив элементов по возрастанию и убывания.
+ * В этом задании вам нужно реализовать сортировку методом выборки.
+ * Смысл этого метод в поиске минимального элемента из оставшихся.
+ */
+public class SortSelected {
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+        int min = MinDiapason.findMin(data, i, data.length-1); // находим мин значение в данном массиве
+            int index = FindLoop.indexOf(data, min, 0, data.length); // находим номер ячейки где лежит мин значение массива
+               int temp = data[i];
+               data[i] = data[index];
+               data[index] = temp;
+        }
+        return data;
+    }
+}
---
+++ chapter_001/src/test/java/array/SortSelectedTest.java
@@ -0,0 +1,39 @@
+package array;
+
+import org.junit.Test;
+import ru.job4j.array.SortSelected;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SortSelectedTest {
+    @Test
+    public void whenSort34125() {
+        int[] input = new int[] {3, 4, 1, 2, 5};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenSort341() {
+        int[] input = new int[] {3, 4, 1};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 3, 4};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenSort841026() {
+        int[] input = new int[] {8, 4, 10, 2, 6};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {2, 4, 6, 8, 10};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenSort34102581() {
+        int[] input = new int[] {3, 4, 10, 2, 5, 8, 1};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 2, 3, 4, 5, 8, 10};
+        assertThat(result, is(expect));
+    }
+
+}
