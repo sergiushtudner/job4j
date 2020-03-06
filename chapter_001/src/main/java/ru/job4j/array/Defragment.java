645423bbab681e68eba7f51544baa21a74d03906
sergiushtudner <sergiu.shtudner@gmail.com>
2020-03-06 03:13:01 +0200

    6.8. Defragmenting an array. [# 234702]

---
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+/**
+ * Метод перемещает заполненные ячейки массива в начало, а пустые в конец.
+ */
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+              int point = index + 1; // указатель, на не null ячейку.
+                  while (point < array.length && array[point] == null) {
+                     point++;
+                 }
+                        if (point < array.length) {
+                            array[index] = array[point];
+                            array[point] = null;
+                        }
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
