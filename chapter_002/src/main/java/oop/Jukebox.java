d21e072c5d53ddcab89537ffa99fef77f568e704
sergiushtudner <sergiu.shtudner@gmail.com>
2020-03-26 16:03:17 +0200

    1.3. Calling a method with arguments. [# 234852]

---
+++ chapter_002/src/main/java/oop/Jukebox.java
@@ -0,0 +1,17 @@
+package oop;
+
+public class Jukebox {
+    public void music(int position){
+        if (position == 1) {
+            System.out.println("Пусть бегут неуклюже");
+        }
+        else if (position == 2) {
+            System.out.println("Спокойной ночи");
+        }
+        else System.out.println("Песня не найдена");
+    }
+    public static void main(String[] args) {
+        Jukebox mbox = new Jukebox();
+        mbox.music(2);
+    }
+}
